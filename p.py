4cd118af9143c4438985f7c2bff3add22a37a1b8
Krunal261035 <krunalgadher250@gmail.com>
2024-07-23 14:03:40 +0530

    Add files via upload

---
+++ p.py
@@ -0,0 +1,39 @@
+# names = "harsh,shubham"
+# print(names[0:5])
+# print(len(names))
+
+fruit = "Mango"
+# mangoLen = len(fruit)
+# print(mangoLen)
+# print(fruit[0:5])
+# print(fruit[1:5])
+# print(fruit[2:5])
+# print(fruit[3:5])
+# print(fruit[4:5]) # this code output is similar to line 14 to 18
+
+# print(fruit[-5:5])
+# print(fruit[-4:5])
+# print(fruit[-3:5])
+# print(fruit[-2:5])
+# print(fruit[-1:5]) this output will be front number are deleted like mango to ango to ngo to go to o
+
+# print(fruit[0:5])
+# print(fruit[0:4])
+# print(fruit[0:3])
+# print(fruit[0:2])
+# print(fruit[0:1]) # this outpt will be back number are deleted like mango to mang to man to ma to m
+
+# print(fruit[-5:-1])
+# print(fruit[-5:-2])
+# print(fruit[-5:-3])
+# print(fruit[-5:-4]) this is similar to line 21 to 25
+
+# print(fruit[-1:-3]) mango
+# print(fruit[-5:-1])
+# print(fruit[-4:-1])
+# print(fruit[-3:-1])
+# print(fruit[-2:-1])
+# print(fruit[-1:-1])
+
+name = "harry"
+print(name[-4:-2])
