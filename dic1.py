34b66944e2e8063d8f7d8c57edc9239df79666ee
Krunal261035 <krunalgadher250@gmail.com>
2024-09-17 12:49:25 +0530

    Add files via upload
    method of dictonary of adding update nested etc

---
+++ dic1.py
@@ -0,0 +1,22 @@
+dict = {}
+print("enter dictonary")
+print(dict)
+dict[0]= 'kuldip'
+dict[1]= 25
+dict[2] = 'harsh'
+print("\nDictonary after adding 3 element")
+print(dict)
+
+dict['score'] = 23,25,30,45
+print("\nDictonary after adding 4 element")
+print(dict)
+
+dict[0] = 'welcome'
+dict[1] = 'to'
+dict[2] = "code with harry"
+print("\nupdate value will be")
+print(dict)
+
+dict[3] = {'Nested': {'1': 'welcome', '2': 'code with harry'}}
+print("\nadding a nested keys ")
+print(dict)
